98fc5337b02f167c5b0917557961ecf029187bad
RajeshBhatt <rjshbhatt9@gmail.com>
2024-06-05 18:37:17 -0700

    manAge.js file added

---
+++ javascript/javascript1/week1/manAge.js
@@ -0,0 +1,9 @@
+//1. Age-ify (A future age calculator)
+
+let yearOfBirth = 1983;
+let futureYear = 2027;
+
+let age = futureYear - yearOfBirth;
+
+//console.log("You will be " +age+ " years old in " +futureYear);
+console.log(`You will be ${age} years old in ${futureYear}.`);
