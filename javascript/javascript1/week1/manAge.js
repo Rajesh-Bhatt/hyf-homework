dd0a3af814b747a1f1922a6a73251a317ed1d13a
RejeshBhatt <rjshbhatt9@gmail.com>
2024-06-06 02:16:01 -0700

    Update manAge.js
    changed variable from let to const.

--- javascript/javascript1/week1/manAge.js
+++ javascript/javascript1/week1/manAge.js
@@ -1,9 +1,9 @@
 //1. Age-ify (A future age calculator)
 
-let yearOfBirth = 1983;
-let futureYear = 2027;
+const yearOfBirth = 1983;
+const futureYear = 2027;
 
-let age = futureYear - yearOfBirth;
+const age = futureYear - yearOfBirth;
 
 //console.log("You will be " +age+ " years old in " +futureYear);
 console.log(`You will be ${age} years old in ${futureYear}.`);
