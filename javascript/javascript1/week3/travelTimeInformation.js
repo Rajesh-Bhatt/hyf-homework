bdf3badec9a93bdbe79353feaa289e1cae416925
RajeshBhatt <rjshbhatt9@gmail.com>
2024-06-20 16:17:11 -0700

    input validation implemented successfully

--- javascript/javascript1/week3/travelTimeInformation.js
+++ javascript/javascript1/week3/travelTimeInformation.js
@@ -5,16 +5,28 @@ const travelInformation = {
   destinationDistance: "432 km",
 };
 
-function calculateTravelTime({ speed, destinationDistance }) {
+function calculateTravelTime({ speed, distance }) {
   const newSpeed = parseInt(travelInformation.speed);
-  const newDestinationDistance = parseInt(
-    travelInformation.destinationDistance
-  );
-  const time = newDestinationDistance / newSpeed;
+  const newDistance = parseInt(travelInformation.destinationDistance);
+  const time = newDistance / newSpeed;
   const hours = Math.floor(time);
   const minutes = Math.floor((time - hours) * 60);
-  return `${hours} hours and ${minutes} minutes`;
-}
 
+  const conditionsCheckArray = [
+    !newSpeed == 0,
+    newSpeed > 0,
+    !newDistance == 0,
+    newDistance > 0,
+    newSpeed === "" || newSpeed <= 200,
+    newDistance === "" || newDistance <= 10000,
+    isNaN.newSpeed || isNaN.newDistance,
+  ];
+
+  if (!conditionsCheckArray.includes(false)) {
+    return `${hours} hours and ${minutes} minutes`;
+  } else {
+    return "Invalid value. Please provide a correct value within a range (speed: 1 - 200 and distance: 1 -10000.";
+  }
+}
 const travelTime = calculateTravelTime(travelInformation);
 console.log(travelTime); // Log out: "8 hours and 38 minutes"
