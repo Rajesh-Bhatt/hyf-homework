36a476d31159c9199b4cab14c92096030b6e6f42
RajeshBhatt <rjshbhatt9@gmail.com>
2024-11-09 19:27:43 -0800

    page.js file added

---
+++ react-2-hw/hw-week2/my-application/app/accordion/page.js
@@ -0,0 +1,10 @@
+import Accordion from "@/app/accordion/Accordion";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={"text-4xl text-center m-5"}>Accordion Page</h1>
+      <Accordion />
+    </div>
+  );
+}
